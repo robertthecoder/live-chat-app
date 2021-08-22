8f045d7ab079acdfbbb8e7ac6c72c41184c95af9
Rj <rj@Rjs-MacBook-Pro.local>
2021-08-21 17:51:49 -0700

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {ChatEngine} from 'react-chat-engine';
+
+import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="f0ef67c1-f0a1-4c18-964e-8364269fc093"
+      userName="javascriptmastery"
+      userSecret="secret"
+      
+
+    />
   );
 }
 
