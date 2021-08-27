379820fdd058cd82e41456628d57df59cd5fe4a0
Rj <rj@Rjs-MacBook-Pro.local>
2021-08-27 14:25:53 -0700

    got avatar pic showing on read receipt

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=jcOKU9f86XE&t=959s
 
 
-CUrrently: 49:00
+CUrrently: 50:09
--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -9,10 +9,10 @@ const ChatFeed = (props) => {
    const {chats, activeChat, userName, messages} = props;
 
     const chat = chats && chats[activeChat];
- 
+  
 
     const renderReadReceipts = (message, isMyMessage) => {
-        chat.people.map((person, index) => person.last_read === message.id && (
+        return chat.people.map((person, index) => person.last_read === message.id && (
             <div 
                 key={`read_${index}`}
                 className = 'read-receipt'
